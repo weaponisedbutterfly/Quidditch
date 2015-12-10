a65a8eeed36060d2450a5cc1b54cdee629d7c79f
Solomon Osiberu <lokobos67@gmail.com>
2015-12-10 08:47:20 +0000

    Added team field to player edit form.

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,6 +4,7 @@ Player.delete_all
 t1 = Team.create!(name: 'Moutohora Macaws', founded: '1 March 1872', mascot: 'Sparky')
 t2 = Team.create!(name: 'Ballycastle Bats', founded: '6 November 1887', mascot: 'Barny the Fruitbat')
 t3 = Team.create!(name: 'Kenmare Kestrels', founded: '24 May 1909', mascot: 'Kez')
+t4 = Team.create!(name: 'None')
 
 Player.delete_all
 
